829f53cdc00d308aeb93dcd1fa2232594a112e93
Luciano Wencel <90798078+Wencel15@users.noreply.github.com>
2023-10-26 15:22:01 -0300

    Update finding-nearby_object.py

--- finding-nearby_object.py
+++ finding-nearby_object.py
@@ -55,3 +55,26 @@ pick_up = Point(46.080074, 38.991289)
 print('cab_26 within the polygon:', cab_26.within(poly))
 print('cab_112 within the polygon:', cab_112.within(poly))
 print('pick_upwithin the polygon:', pick_up.within(poly))
+
+#Simulando pontos de entradas com a divisoria do mapa contendo um rio
+
+from shapely.geometry import Point, Polygon
+from geopy.distance import distance
+
+#utilizaremos as mesmas coordenadas do exemplo anterior, incluindo o ponto de entrada do rio
+
+coords = [(46.082991, 38.987384),(46.075489, 38.987599),(46.079395, 38.997684),(46.073822, 39.007297),(46.081741, 39.008842)]
+
+poly = Polygon(coords)
+cab_26 = Point(46.073852, 38.991890)
+cab_112 = Point(46.078228, 39.003949)
+pick_up = Point(46.080074, 38.991289)
+entry_point = Point(46.075357, 39.000298)
+
+if cab_26.within(poly):
+    dist = distance((pick_up.x, pick_up.y),(cab_26.x, cab_26.y)).m
+else:
+    dist = distance((cab_26.x, cab_26.y), (entry_point.x, entry_point.y)).m + distance((entry_point.x, entry_point.y), (pick_up.x, pick_up.y)).m
+    
+print(round(dist)) # calculamos o distancia entre o taxi e o ponto de entrada(se estiver fora do limite do poligono rio), depois a entrada do ponto de entrada até o local desejado
+
